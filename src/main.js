780fec81ae5e0ccfa0dc9286bd104618633af9bf
non000 <1903225251@qq.com>
2021-06-10 13:55:08 +0800

    Initialize the project

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import store from './store'
+import "./permission"
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
